32c3850f0a9bbe9c3589e1bd81f7048d4d64511c
konnn04 <trieukon1011@gmail.com>
2024-03-08 10:48:47 +0700

    final

--- access/main.js
+++ access/main.js
@@ -23,21 +23,15 @@ music.volume = 0.3
 music.currentTime = 3
 // music.autoplay  =true
 
-function togglePlay() {
-  if (FLAG.music){
-    return
-  }else{
-    try {
-      music.play()
-      FLAG.music = true
-      document.body.removeEventListener("click",togglePlay())
-    } catch (error) {
-      
-    }
+
+document.body.onclick = async()=>{
+  try {
+    await music.play()
+    FLAG.music = true
+  } catch (error) {
+    
   }
 }
-
-document.body.addEventListener("click",togglePlay())
 
 const text = `
   Hôm nay chính là một ngày rất đặc biệt - ngày 8/3 hay còn được biết tới với cái tên là ngày Quốc tế Phụ nữ! Bên cạnh những lời yêu thương vô bờ bến, CLB Lập trình trên thiết bị di động cũng xin gửi lời chúc đến tất cả các bạn nữ và các cô giảng viên sẽ luôn luôn thành công trong công việc, hạnh phúc trong tình yêu và luôn nở rộ như những đóa hoa xinh đẹp ❤❤
