ac7f12a6c38ab701d0992ae451aed20fcb60cea3
konnn04 <trieukon1011@gmail.com>
2024-03-08 10:11:04 +0700

    final

--- access/main.js
+++ access/main.js
@@ -13,7 +13,7 @@ const FLAG = {
 }
 
 const text = `
-  Hôm nay chính là một ngày rất đặc biệt - ngày 8/3 hay còn được biết tới với cái tên là ngày Quốc tế Phụ nữ! Bên cạnh những lời yêu thương vô bờ bến, LB Lập trình trên thiết bị di động cũng xin gửi lời chúc đến tất cả các bạn nữ và các cô giảng viên sẽ luôn luôn thành công trong công việc, hạnh phúc trong tình yêu và luôn nở rộ như những đóa hoa xinh đẹp ❤❤
+  Hôm nay chính là một ngày rất đặc biệt - ngày 8/3 hay còn được biết tới với cái tên là ngày Quốc tế Phụ nữ! Bên cạnh những lời yêu thương vô bờ bến, CLB Lập trình trên thiết bị di động cũng xin gửi lời chúc đến tất cả các bạn nữ và các cô giảng viên sẽ luôn luôn thành công trong công việc, hạnh phúc trong tình yêu và luôn nở rộ như những đóa hoa xinh đẹp ❤❤
 `.trim()
 
 //Onclick
